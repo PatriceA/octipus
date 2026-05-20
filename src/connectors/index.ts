8a13ca251731bfef1c5b91949103cc01ee737cfa
pallegue <patrice@fintus.de>
2026-05-20 15:12:26 +0200

    feat(connectors): add connector module index

---
+++ src/connectors/index.ts
@@ -0,0 +1,4 @@
+export { OAuthHTTPTransport } from './oauth-http-transport';
+export { ATLASSIAN_CONNECTOR, ATLASSIAN_OAUTH_DISCOVERY_URL } from './atlassian/definition';
+export { ConnectorRegistry, getConnectorRegistry } from './registry';
+export type { ConnectorDefinition, UserConnectorStatus } from './types';
